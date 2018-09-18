ba81f79bd17609e436e10d7c7c44d58af8d9247a
Yekaterina Doros <yekaterinadoros@gmail.com>
2018-09-18 19:56:18 -0400

    sass

--- public/client.js
+++ public/client.js
@@ -1,58 +1,13 @@
-let activeUserEmail = "";
-let activeUserName = "";
-let activeUserId = "";
+window.onbeforeunload = function () {
+    window.scrollTo(0,0);
+};
 
 $(document).ready(function(){
-    $("#home-page").show();
-    $("#shop").hide();
-})
-
-$("#signUp-form").submit(function(event){
-    event.preventDefault();
-
-    let name = $("#create_name").val();
-    let email = $("#create_email").val();
-    let password = $("#create_password").val();
-//    console.log(signUpName, signUpEmail, signUpPassword);
-
-    if ((!name) || (name.length < 1)) {
-        alert('Invalid username');
-    } else if ((!email) || (email.length < 1) || (email.indexOf(' ') > 0)) {
-        alert('Invalid email');
-    } else if ((!password) || (password.length < 1) || (password.indexOf(' ') > 0)) {
-        alert('Invalid password');
-    } else newUserObject = {
-        name: name,
-        email: email,
-        password: password
-    };
-
-    $.ajax({
-        type: "POST",
-        url: '/users_create',
-        dataType: 'json',
-        data: JSON.stringify(newUserObject),
-        contentType: 'application/json'
-    })
-
-    .done ( function(results) {
-        console.log(results);
-        activeUserEmail = results.email;
-        activeUserName = results.name;
-        activeUserId = results.id;
-//        showOrders(activeUserEmail);
-        $("#home-page").hide();
-        $("#shop").show();
-
-    })
-
-        .fail( function (jqXHR, error, errorThrown) {
-        console.log(jqXHR);
-        console.log(error);
-        console.log(errorThrown);
-        alert('Invalid username and password combination. Check your username and password and try again.');
-    })
+    $(window).scroll(function(){
+        $(".tag-lines").children().slideDown(2000, function(){
+            $(".tag-lines").children().animate({'height': '300px'});
+            $(".tag-lines").css({'background-color': '#EE523F'});
+        });
+    });
 
 })
-
-
