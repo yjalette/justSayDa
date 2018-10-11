30476c2ee50d1788560ab0fdaf7039f24e9ac0da
Yekaterina Doros <yekaterinadoros@gmail.com>
2018-10-11 17:37:05 -0400

    t

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require('mysql')
 const router = express.Router();
 const app = express();
-const bcrypt = require('../app.js').bcrypt();
+
 
 app.use(express.static(__dirname + '/public'));
 
